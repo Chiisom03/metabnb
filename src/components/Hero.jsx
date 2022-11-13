8673d8fb55ba0d7209b9be132bc9dc2b871d09d1
Peter Chisom <peterchisom69@gmail.com>
2022-11-13 13:07:31 +0100

    feat: created hero component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Hero = () => {
+    return (
+        <div>
+            <div>
+                <h2>Rent a Place away from Home in Metaverse</h2>
+            </div>
+        </div>
+    );
+}
+
+export default Hero;
