0521c3ff929050346f26faa38c0b6e977a3e3cc8
Peter Chisom <peterchisom69@gmail.com>
2022-11-14 15:22:07 +0100

    style: fixed font-weight issues

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,12 +3,12 @@ import heroImg from "../assets/images/hero_img.png";
 
 const Hero = () => {
   return (
-    <div className=" mt-[125px] h-[495px] max-w-[1221px] mx-[100px] flex">
+    <div className=" mt-[125px] h-[495px] max-w-[1221px] mx-[100px] flex text-metablack">
       <div className=" mr-[99px]">
-        <h2 className="text-[56px] ">
-          Rent a <span className="text-major">Place</span> away from{" "}
-          <span className="text-major">Home</span> in{" "}
-          <span className="text-major">Metaverse</span>
+        <h2 className="text-[56px] leading-[78.4px] tracking-[-2%]">
+          Rent a <span className="text-major font-bold">Place</span> away from{" "}
+          <span className="text-major font-bold">Home</span> in{" "}
+          <span className="text-major font-bold">Metaverse</span>
         </h2>
         <p className="text-2xl">
           we provide you access to luxury and affordable houses in the
