96bfc109cb2ff85efb2835659b0b20ea60fabbf8
Peter Chisom <peterchisom69@gmail.com>
2022-11-14 21:43:04 +0100

    chore: imported heart img

--- src/components/Heart.jsx
+++ src/components/Heart.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import heart from '../assets/images/heart.png'
 
 const Heart = () => {
     return (
         <div>
-            
+            <img src={heart} alt="" />
         </div>
     );
 }
