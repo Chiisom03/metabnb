7ee2d71e36a8f50b2ca1f3c1bc5e3c15500d0fde
Peter Chisom <peterchisom69@gmail.com>
2022-11-14 20:40:41 +0100

    style: added brands style

--- src/components/Brands.jsx
+++ src/components/Brands.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
+import mbtoken from "../assets/images/MBToken.png";
+import metamask from "../assets/images/metamask.png";
+import opensea from "../assets/images/opensea.png";
 
 const Brands = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-}
+  const brands = [
+    { name: mbtoken },
+    { name: metamask },
+    { name: opensea },
+  ];
+  return (
+    <div className="h-[70px] w-full bg-major flex justify-around py-[14.61px] mt-[67px]">
+      {brands.map((brand) => (
+        <img src={brand.name} alt="mbtoken_logo" className="h-[40.9px] w-[198.32px]" />
+      ))}
+    </div>
+  );
+};
 
 export default Brands;
