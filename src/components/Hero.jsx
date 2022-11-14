7e29e63f2a39181513f36be6dc83ac7f4548df5c
Peter Chisom <peterchisom69@gmail.com>
2022-11-14 11:54:20 +0100

    style: adjusted spacing

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,20 +3,24 @@ import heroImg from "../assets/images/hero_img.png";
 
 const Hero = () => {
   return (
-    <div className=" mt-[125px] h-[495px] max-w-[1221px] mx-auto flex">
+    <div className=" mt-[125px] h-[495px] max-w-[1221px] mx-[100px] flex">
       <div className=" mr-[99px]">
-        <h2 className="text-[56px]">
+        <h2 className="text-[56px] ">
           Rent a <span className="text-major">Place</span> away from{" "}
           <span className="text-major">Home</span> in{" "}
           <span className="text-major">Metaverse</span>
         </h2>
-        <p className="text-xs">
+        <p className="text-2xl">
           we provide you access to luxury and affordable houses in the
           metaverse, get a chance to turn your imagination to reality at your
           comfort zone
         </p>
       </div>
-      <img src={heroImg} alt="hero_img" className="h-[476px] w-[459px]" />
+      <img
+        src={heroImg}
+        alt="hero_img"
+        className="h-[476px] w-[459px] mr-[19px]"
+      />
     </div>
   );
 };
