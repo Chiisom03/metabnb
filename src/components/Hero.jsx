a7a97d542ddf8435d20c72123d0e1db24763b26e
Peter Chisom <peterchisom69@gmail.com>
2022-11-14 11:11:09 +0100

    style: styled hero main text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React from "react";
 
 const Hero = () => {
-    return (
-        <div>
-            <div>
-                <h2>Rent a Place away from Home in Metaverse</h2>
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className="">
+      <div>
+        <h2 className="text-[56px]">
+          Rent a <span className="text-major">Place</span> away from{" "}
+          <span className="text-major">Home</span> in{" "}
+          <span className="text-major">Metaverse</span>
+        </h2>
+        <p className="text-xs">
+          we provide you access to luxury and affordable houses in the
+          metaverse, get a chance to turn your imagination to reality at your
+          comfort zone
+        </p>
+      </div>
+      <img src="" alt="" />
+    </div>
+  );
+};
 
 export default Hero;
