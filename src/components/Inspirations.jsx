eb82aadced42f2928b9913431284f1618ffa3d8d
Peter Chisom <peterchisom69@gmail.com>
2022-11-15 03:47:17 +0100

    feat: made banner section responsive

--- src/components/Inspirations.jsx
+++ src/components/Inspirations.jsx
@@ -5,11 +5,11 @@ import { images } from "../nfts";
 
 const Inspirations = () => {
   return (
-    <div className="mt-[58px] mx-[100px]">
-      <h2 className="text-5xl font-bold mb-[43px] text-center">
+    <div className="mt-[58px] md:mx-[100px] mx-4">
+      <h2 className="text-4xl md:text-5xl font-bold mb-[43px] text-center">
         Inspiration for your next adventure
       </h2>
-      <div className="grid grid-cols-4 gap-6 mb-[53px]">
+      <div className="grid grid-cols-1 place-items-center gap-y-6 md:grid-cols-4 md:gap-6 mb-[53px]">
         {images.map((i) => (
           <NFT key={i.link} image={inspiro} />
         ))}
