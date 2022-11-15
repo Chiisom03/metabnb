67a91cf564e65d48ab01f21385274638c6a89dd0
Peter Chisom <peterchisom69@gmail.com>
2022-11-15 02:19:34 +0100

    feat: created footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="h-[322px] w-full px-[113px] pt-[62.88px] pb-[37px] bg-[#1D1D1E]">
+      <div className="mb-[96px]">
+        <img src="" alt="metabnb logo" className="" />
+        <ul className="mb-[59px]">
+          <li>facebook</li>
+        </ul>
+        <p>&copy; 2022 Metabnb </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
