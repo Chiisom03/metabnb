1e13b5b1716ff12f60408fb4b72b575f344734f2
Peter Chisom <peterchisom69@gmail.com>
2022-11-15 02:22:31 +0100

    chore: included footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Banner from "./components/Banner";
 import Brands from "./components/Brands";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Inspirations from "./components/Inspirations";
@@ -12,6 +13,7 @@ function App() {
       <Brands />
       <Inspirations />
       <Banner />
+      <Footer />
     </div>
   );
 }
