91b77fdfcc3938c8d437dd8b7c8a9a16d3de6223
Peter Chisom <peterchisom69@gmail.com>
2022-11-15 14:09:54 +0100

    feat: made hero section responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,14 +4,14 @@ import Search from "./Search";
 
 const Hero = () => {
   return (
-    <div className=" mt-[125px] h-[495px] max-w-[1221px] mx-[100px] flex text-metablack">
-      <div className=" mr-[99px]">
-        <h2 className="text-[56px] leading-[78.4px] tracking-[-2%]">
-          Rent a <span className="text-major font-bold">Place</span> away from{" "}
-          <span className="text-major font-bold">Home</span> in{" "}
-          <span className="text-major font-bold">Metaverse</span>
+    <div className="mt-[30px] md:mt-[125px] h-[495px] w-full md:max-w-[960px] px-6 md:mx-auto md:flex flex-col md:flex-row md:items-center md:justify-center text-metablack">
+      <div className="lg:mr-[99px] md:mr-[50px]">
+        <h2 className="text-5xl md:text-[56px] md:leading-[78.4px] md:tracking-[-2%]">
+          Rent a <span className="text-major font-bold">Place </span> away from
+          <span className="text-major font-bold"> Home</span> in
+          <span className="text-major font-bold"> Metaverse</span>
         </h2>
-        <p className="text-2xl mt-12">
+        <p className="text-xl md:text-2xl mt-12">
           we provide you access to luxury and affordable houses in the
           metaverse, get a chance to turn your imagination to reality at your
           comfort zone
@@ -21,7 +21,7 @@ const Hero = () => {
       <img
         src={heroImg}
         alt="hero_img"
-        className="h-[476px] w-[459px] mr-[19px]"
+        className="h-[476px] w-[459px] mr-[19px] hidden md:block"
       />
     </div>
   );
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const Search = () => {
   return (
-    <div className="mt-12 w-[618.7px] flex">
+    <div className="mt-12 w-full md:w-[618.7px] flex">
       <input
         type="text"
-        className="py-[17px] pl-[15px] border border-metagray outline-none rounded-tl-lg rounded-bl-lg"
+        className="w-[70%] md:py-[17px] pl-[15px] border border-metagray outline-none rounded-tl-lg rounded-bl-lg"
         placeholder="Search for location"
       />
-      <a href="/" className="bg-major w-[230px] py-[17px] px-[87px] rounded-tr-lg rounded-br-lg text-white">Search</a>
+      <a href="/" className="bg-major w-[30%] md:w-[230px] py-[10px] px-[30px] md:py-[17px] md:px-[87px] rounded-tr-lg rounded-br-lg text-white">Search</a>
     </div>
   );
 };
