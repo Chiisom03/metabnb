911a7e0572dd888dfd086d8766913347a80e4df9
Peter Chisom <peterchisom69@gmail.com>
2022-11-15 02:09:54 +0100

    style: added space between inspiration and banner

--- src/components/Inspirations.jsx
+++ src/components/Inspirations.jsx
@@ -9,7 +9,7 @@ const Inspirations = () => {
       <h2 className="text-5xl font-bold mb-[43px] text-center">
         Inspiration for your next adventure
       </h2>
-      <div className="grid grid-cols-4 gap-6">
+      <div className="grid grid-cols-4 gap-6 mb-[53px]">
         {images.map((link) => (
           <NFT image={inspiro} />
         ))}
