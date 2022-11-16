480a8c9fe0f36620711594c63b7f4fbedb1f793e
Peter Chisom <peterchisom69@gmail.com>
2022-11-16 04:41:29 +0100

    chore: seperated concerns for home page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
-import Banner from "./components/Banner";
-import Brands from "./components/Brands";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import Hero from "./components/Hero";
-import Inspirations from "./components/Inspirations";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="font-rose">
       <Header />
-      <Hero />
-      <Brands />
-      <Inspirations />
-      <Banner />
+      <Home />
       <Footer />
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React from "react";
+import Banner from "../components/Banner";
+import Brands from "../components/Brands";
+import Hero from "../components/Hero";
+import Inspirations from "../components/Inspirations";
 
 const Home = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-}
+  return (
+    <>
+      <Hero />
+      <Brands />
+      <Inspirations />
+      <Banner />
+    </>
+  );
+};
 
 export default Home;
